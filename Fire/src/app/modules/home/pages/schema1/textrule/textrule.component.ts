eefb20fbee70c2a6cf8ff22a15a1bb3307978d02
roman <aboandrus@gmail.com>
2019-10-17 21:23:29 +0200

    Created resistanceTable json

--- Fire/src/app/modules/home/pages/schema1/textrule/textrule.component.ts
+++ Fire/src/app/modules/home/pages/schema1/textrule/textrule.component.ts
@@ -12,4 +12,13 @@ export class TextruleComponent implements OnInit {
   ngOnInit() {
   }
 
+  resistanceTable ={ ZLI_N:"D,C,B", ZLII_N:"D,C,B", ZLIII_N:"D,D,C", ZLIV_N:"D", ZLV_N:"C",
+                      ZLI_SW:"B", ZLII_SW:"B", ZLIII_SW:"B", ZLIV_SW:"C", ZLV_SW:"B",
+                      ZLI_W:"B", ZLII_W:"B", ZLIII_W:"B", ZLIV_W:"B", ZLV_W:"B",
+                      ZLI_WW:"A", ZLII_WW:"A", ZLIII_WW:"A", ZLIV_WW:"A", ZLV_WW:"A" }
+
+  getValueFromResistanceTable(value: string){
+  return this.resistanceTable[value]
+  }
+
 }
