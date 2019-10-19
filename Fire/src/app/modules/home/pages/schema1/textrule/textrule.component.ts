dd70222f357c496d7b99d32e4082b0231a74e0e6
roman <aboandrus@gmail.com>
2019-10-19 16:40:32 +0200

    Implmented text shows

--- Fire/src/app/modules/home/pages/schema1/textrule/textrule.component.ts
+++ Fire/src/app/modules/home/pages/schema1/textrule/textrule.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input  } from '@angular/core';
+import { MatTreeFlatDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-textrule',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextruleComponent implements OnInit {
 
+  @Input() passInfo: string;
+
+  showTextForA: Boolean = false;
+  showTextForB: Boolean = false;
+  showTextForC: Boolean = false;
+  showTextForD: Boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(){
+    if (this.getValueFromResistanceTable( this.passInfo ) == "A"){
+      this.setAllFalse()
+      this.showTextForA = true;
+    } 
+    if (this.getValueFromResistanceTable( this.passInfo ) == "B"){
+      this.setAllFalse()
+      this.showTextForB = true;
+    } 
+    if (this.getValueFromResistanceTable( this.passInfo ) == "C"){
+      this.setAllFalse()
+      this.showTextForC = true;
+    } 
+    if (this.getValueFromResistanceTable( this.passInfo ) == "D"){
+      this.setAllFalse()
+      this.showTextForD = true;
+    }
   }
 
   resistanceTable ={ ZLI_N:"D,C,B", ZLII_N:"D,C,B", ZLIII_N:"D,D,C", ZLIV_N:"D", ZLV_N:"C",
@@ -20,5 +47,16 @@ export class TextruleComponent implements OnInit {
   getValueFromResistanceTable(value: string){
   return this.resistanceTable[value]
   }
+
+  setAllFalse(){
+    this.showTextForA = false;
+    this.showTextForB = false;
+    this.showTextForC = false;
+    this.showTextForD = false;
+  }
+
+
+ 
+
 
 }
