3d2fc67973b3609911ce7b684f2dfb60c6a7e592
roman <aboandrus@gmail.com>
2019-10-30 20:45:38 +0100

    Removed unused

--- Fire/src/app/app.module.ts
+++ Fire/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './modules/home/nav/nav.component';
@@ -8,8 +7,6 @@ import { Schema1Component } from './modules/home/pages/schema1/schema1.component
 import { Schema2Component } from './modules/home/pages/schema2/schema2.component';
 import { Schema3Component } from './modules/home/pages/schema3/schema3.component';
 import { ContactComponent } from './modules/home/pages/contact/contact.component';
-import { HorButtonGroupComponent } from './modules/home/pages/schema1/zl/hor-button-group/hor-button-group.component';
-import { VerButtonGropuComponent } from './modules/home/pages/schema1/zl/ver-button-gropu/ver-button-gropu.component';
 import { ZlButtonComponent } from './modules/home/pages/schema1/zl/hor-button-group/zl-button/zl-button.component';
 import { HeightButtonComponent } from './modules/home/pages/schema1/zl/ver-button-gropu/height-button/height-button.component';
 import { TableComponent } from './modules/home/pages/schema1/zl/table/table.component';
@@ -17,18 +14,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { MatSliderModule } from '@angular/material/slider';
-
 import {MatGridListModule} from '@angular/material'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { TextruleComponent } from './modules/home/pages/schema1/textrule/textrule.component';
-import { TablePMComponent } from './modules/home/pages/schema1/table-pm/table-pm.component';
-import { VerButtonGroupPMComponent } from './modules/home/pages/schema1/ver-button-group-pm/ver-button-group-pm.component';
-import { HorButtonGroupPMComponent } from './modules/home/pages/schema1/hor-button-group-pm/hor-button-group-pm.component';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import { ZlComponent } from './modules/home/pages/schema1/zl/zl.component';
 import { PmComponent } from './modules/home/pages/schema1/pm/pm.component';
-import { KlasatableComponent } from './modules/home/pages/schema1/klasatable/klasatable.component';
-
 
 @NgModule({
   declarations: [
@@ -38,20 +29,12 @@ import { KlasatableComponent } from './modules/home/pages/schema1/klasatable/kla
     Schema2Component,
     Schema3Component,
     ContactComponent,
-    HorButtonGroupComponent,
-    VerButtonGropuComponent,
     ZlButtonComponent,
     HeightButtonComponent,
     TableComponent,
     TextruleComponent,
-    TablePMComponent,
-    VerButtonGroupPMComponent,
-    HorButtonGroupPMComponent,
     ZlComponent,
     PmComponent,
-    KlasatableComponent
-    
-    
   ],
   imports: [
     BrowserModule,
